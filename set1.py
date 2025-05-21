f6bd7c2cd7fb93168850001a3c60547b4e8969f1
Rashigs17 <rashigsuvarna@gmail.com>
2025-05-21 15:13:40 +0530

    Add files via upload

---
+++ set1.py
@@ -0,0 +1,14 @@
+s1 = {1, 1, 1, 2, 3, 4, 4, 5}
+print("s1:", s1)  
+
+s2 = {101, 103, 105, 105, 102}
+print("s2:", s2)  
+print("s1 & s2 (intersection):", s1 & s2)
+
+
+print("s1 | s2 (union):", s1 | s2)
+
+
+print("s1 - s2 (difference):", s1 - s2)
+
+print("s1 ^ s2 (symmetric difference):", s1 ^ s2)
